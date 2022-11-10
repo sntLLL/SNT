e021088deea8d57f0a52d7cd643e79541c6b6978
sntLLL <114366397+sntLLL@users.noreply.github.com>
2022-11-10 09:02:43 +0100

    Add files via upload

---
+++ seance03_test01/construire.py
@@ -0,0 +1,9 @@
+import sys
+# On définit l'emplacement du ficier preparation :
+new = "..\\preparation"
+#new = "..//preparation"
+sys.path.append(new)
+# puis on l'importe
+from prep import*
+# on lance la construction du notebook
+construire('seance03.txt')
